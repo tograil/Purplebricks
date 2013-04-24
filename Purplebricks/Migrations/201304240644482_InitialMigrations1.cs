44f42440ee654d01c9461ddb534ecba1b64ca236
Andrey <tograilx@ukrpost.net>
2013-04-24 09:48:02 +0300

    database

---
+++ Purplebricks/Migrations/201304240644482_InitialMigrations1.cs
@@ -0,0 +1,16 @@
+namespace Purplebricks.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrations1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
